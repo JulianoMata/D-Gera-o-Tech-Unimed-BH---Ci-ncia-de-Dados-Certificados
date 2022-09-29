b6bae2316be96e034d7949cc05357da7ebbc5b46
JulianoMata <jfmatta@gmail.com>
2022-09-29 19:29:43 -0300

    Arquivo 'identacao_blocos' adicionado

---
+++ Scripts/Curso_04/identacao_blocos.py
@@ -0,0 +1,13 @@
+def sacar(valor):
+    saldo = 500
+    if saldo >= valor:
+        print("Valor sacado!")
+        print("Retire o seu dinheiro.")
+    print("Obrigado, bom dia!!!")
+    
+    
+def depositar(valor):
+    saldo = 500
+    saldo += valor
+        
+sacar(100)
