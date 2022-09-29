296dc62a835a148bc3d388fd7369a06f7525ce76
JulianoMata <jfmatta@gmail.com>
2022-09-29 17:27:39 -0300

    Arquivo 'operadores_aritmeticos'adicionado e correção nome 'convertendo'

---
+++ Scripts/operadores_aritmeticos.py
@@ -0,0 +1,10 @@
+numero_1 = 20
+numero_2 = 7
+
+print(f'A soma de {numero_1} e {numero_2} é {numero_1 + numero_2}\n')
+print(f'A subtração de {numero_1} por {numero_2} é {numero_1 - numero_2}\n')
+print(f'A divisão de {numero_1} por {numero_2} é {numero_1 / numero_2}\n')
+print(f'A divisão inteira de {numero_1} por {numero_2} é {numero_1 // numero_2}\n')
+print(f'A multiplicação de {numero_1} por {numero_2} é {numero_1 * numero_2}\n')
+print(f'A exponenciação de {numero_1} por {numero_2} é {numero_1 ** numero_2}\n')
+print(f'O módulo (resto) de {numero_1} por {numero_2} é {numero_1 % numero_2}\n')
