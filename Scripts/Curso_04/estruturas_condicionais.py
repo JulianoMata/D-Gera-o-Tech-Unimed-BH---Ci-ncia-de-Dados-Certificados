3e215bb7d9f0c948cde8eb521ccd1e4796f67caf
JulianoMata <jfmatta@gmail.com>
2022-09-29 20:09:16 -0300

    Arquivo 'estruturas_condicionais' adicionado

--- Scripts/Curso_04/estruturas_condicionais.py
+++ Scripts/Curso_04/estruturas_condicionais.py
@@ -45,4 +45,30 @@ if idade >= MAIOR_IDADE:
 
 if idade < MAIOR_IDADE:
     print("Ainda não pode tirar CNH.")
+   
+#------------------------------------------------------------------------------------------------------------ 
+    
+MAIOR_IDADE = 18
 
+idade = int(input("Informe sua idade: "))
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar CNH.")
+
+else:
+    print("Ainda não pode tirar CNH.")
+
+#------------------------------------------------------------------------------------------------------------
+
+MAIOR_IDADE = 18
+IDADE_ESPECIAL = 17
+
+idade = int(input("Informe sua idade: "))
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar CNH.")
+    
+elif idade == IDADE_ESPECIAL:
+    print("Pode fazer aulas teóricas, mas não pode tirar CNH.")
+
+else:
+    print("Ainda não pode tirar CNH.")
