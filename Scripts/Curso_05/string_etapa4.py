dba78e2c537732e20dc7d7d019fab4b48db11685
JulianoMata <jfmatta@gmail.com>
2022-09-30 20:38:20 -0300

    Tetsando cores no código

--- Scripts/Curso_05/string_etapa4.py
+++ Scripts/Curso_05/string_etapa4.py
@@ -2,7 +2,7 @@ nome = "Juliano"
 curso = "Python"
 
 mensagem = f'''
-    Olá, meu nome é {nome}.
+    Olá, meu nome é \033[1;31m{nome}\033[m.
 Estou revendo {curso}.
         Mensagem com diferentes recuos.
 '''
@@ -10,13 +10,13 @@ print(mensagem)
 
 
 print("""
-      ============ MENU ============
+      \033[1;31m============ MENU ============\033[m
       
-      [1] Depositar
-      [2] Sacar
-      [3] Sair
+      \033[1;32m[1] Depositar\033[m
+      \033[1;33m[2] Sacar\033[m
+      \033[1;34m[3] Sair\033[m
       
-      ==============================
+      \033[1;31m==============================\033[m
       
-    Obrigado por usar nosso sistema!!!
+     \033[1;35;42mObrigado por usar nosso sistema!!!\033[m
       """)
