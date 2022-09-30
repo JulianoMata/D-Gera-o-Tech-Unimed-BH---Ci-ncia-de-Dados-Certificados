2bf73039a908d68ed5bcf4ac7ab1102d9f6eada2
JulianoMata <jfmatta@gmail.com>
2022-09-30 18:35:11 -0300

    'string_etapa4' adicionado

---
+++ Scripts/Curso_05/string_etapa4.py
@@ -0,0 +1,22 @@
+nome = "Juliano"
+curso = "Python"
+
+mensagem = f'''
+    Olá, meu nome é {nome}.
+Estou revendo {curso}.
+        Mensagem com diferentes recuos.
+'''
+print(mensagem)
+
+
+print("""
+      ============ MENU ============
+      
+      [1] Depositar
+      [2] Sacar
+      [3] Sair
+      
+      ==============================
+      
+    Obrigado por usar nosso sistema!!!
+      """)
