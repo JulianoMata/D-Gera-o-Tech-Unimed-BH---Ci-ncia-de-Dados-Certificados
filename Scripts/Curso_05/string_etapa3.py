2eb579313e86130d71fb5b53bebee5dad1c27018
JulianoMata <jfmatta@gmail.com>
2022-09-30 17:12:36 -0300

    'string_etapa3' adicionado

---
+++ Scripts/Curso_05/string_etapa3.py
@@ -0,0 +1,18 @@
+nome = "Juliano França da Mata"
+
+print(nome[0])
+
+print(nome[-14])
+
+print(nome[:8])
+
+print(nome[8:])
+
+print(nome[8:14])
+
+print(nome[8:14:2])
+
+print(nome[:])
+
+print(nome[::-1])
+
