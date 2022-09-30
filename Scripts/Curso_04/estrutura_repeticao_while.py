4ac259148907d0b0d85bcb71fae97e6ea40a54f8
JulianoMata <jfmatta@gmail.com>
2022-09-29 21:09:09 -0300

    'estrutura_repeticao' adicionado

---
+++ Scripts/Curso_04/estrutura_repeticao_while.py
@@ -0,0 +1,14 @@
+opcao = -1
+
+while opcao != 0:
+    opcao = int(input("\n[1] Sacar\[2] Extrato \[0] Sair \n: "))
+    
+    if opcao == 1:
+        print("Contando cédulas...")
+        
+    elif opcao == 2:
+        print("exibindo o extrato...")
+
+else:
+    print("Obrigado, volte sempre!!!")
+        
