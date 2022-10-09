055dc03b3b6906face9cfeb7193c418703976fd2
JulianoMata <jfmatta@gmail.com>
2022-10-09 19:59:05 -0300

    desafio de codigo 'aumento salarial'

---
+++ Scripts/Desafio_de_codigo/desafio_de_codigo_aumento_salarial.py
@@ -0,0 +1,63 @@
+""" 
+Desafios Intermediários Py - Unimed-BH
+3 / 3 - Aumento Salarial
+
+
+
+
+
+ Básico
+ Porcentagem
+A empresa que você trabalha resolveu conceder um aumento salarial a todos funcionários, de acordo com a tabela abaixo:
+
+Salário	            Percentual de Reajuste
+
+0 - 600.00                  17%
+600.01 - 900.00             13%
+900.01 - 1500.00            12%
+1500.01 - 2000.00           10%
+Acima de 2000.00             5%
+
+Leia o salário do funcionário e calcule e mostre o novo salário, bem como o valor de reajuste ganho e o índice reajustado, em percentual.
+
+A entrada contém apenas um valor de ponto flutuante, que pode ser maior ou igual a zero, com duas casas decimais, conforme exemplos abaixo.
+
+Exemplo 1
+
+Entrada	        Saída
+
+1000	        Novo salario: 1120,00 
+                Reajuste ganho:120,00                                                                                     
+                Em percentual: 12 %
+ 
+
+Exemplo 2
+
+Entrada	        Saída
+
+2000	        Novo salario: 2200,00                                                                                                   
+                Reajuste ganho: 200,00                                                                                                  
+                Em percentual: 10 %
+
+"""
+salario = float(input(f"Valor salário: "))
+
+if (salario <= 600):
+    percentual = 17
+elif (salario <= 900):
+    percentual = 13
+elif (salario <= 1500):
+    percentual = 12
+elif (salario <= 2000):
+    percentual = 10
+else:
+    percentual = 5
+
+reajuste_ganho = (percentual * salario) / 100
+novo_salario = (salario + reajuste_ganho)
+
+print(f"Novo salario: {(novo_salario):.2f}\nReajuste ganho: {reajuste_ganho:.2f}\nEm percentual: {percentual} %".replace(".",","))    
+# print(f"Reajuste ganho: R$ {reajuste_ganho}")
+# print()
+# print(f"Em percentual: {percentual}%")
+
